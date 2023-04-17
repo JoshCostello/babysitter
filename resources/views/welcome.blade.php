87a8be9e620779e11a852db2c4d424e6b3848719
Joshua Costello <costeljm@gmail.com>
2023-04-17 06:51:14 -0400

    Adds Tailwind CSS; Adds simple style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,56 +4,62 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        @vite('resources/css/app.css')
+
         <title>Babysitter Kata</title>
     </head>
     <body class="antialiased">
-        <main>
-            <h1>Babysitter Kata</h1>
-            <form action="{{ route('calculate') }}" method="POST">
+        <main class="px-24 py-12">
+            <header class="mb-12">
+                <h1 class="text-center text-3xl font-bold">Babysitter Kata</h1>
+            </header>
+
+            <form class="max-w-md mx-auto" action="{{ route('calculate') }}" method="POST">
                 @csrf
 
                 @error('calculation')
-                    <p>{{ $message }}</p>
+                    <p class="bg-red-100 font-bold text-red-600 px-4 py-2 rounded-lg mb-8">{{ $message }}</p>
                 @enderror
 
-                <fieldset>
-                    <div>
-                        <label for="arrival">Arrival Time</label>
+                <fieldset class="mb-4">
+                    <div class="mb-4">
+                        <label class="block font-bold mb-1" for="arrival">Arrival Time <span class="text-gray-400">(Required)</span></label>
 
-                        <input id="arrival" name="arrival" value="{{ old('arrival') }}" type="datetime-local">
+                        <input class="border-2 rounded-md px-2 py-1 text-lg" id="arrival" name="arrival" value="{{ old('arrival') }}" type="datetime-local">
 
                         @error('arrival')
-                            <p>{{ $message }}</p>
+                            <p class="text-red-400 font-bold text-sm">{{ $message }}</p>
                         @enderror
                     </div>
 
-                    <div>
-                        <label for="bedtime">Bedtime</label>
+                    <div class="mb-4">
+                        <label class="block font-bold mb-1" for="bedtime">Bedtime</label>
 
-                        <input id="bedtime" name="bedtime" value="{{ old('bedtime') }}" type="datetime-local">
+                        <input class="border-2 rounded-md px-2 py-1 text-lg" id="bedtime" name="bedtime" value="{{ old('bedtime') }}" type="datetime-local">
 
                         @error('bedtime')
-                            <p>{{ $message }}</p>
+                            <p class="text-red-400 font-bold text-sm">{{ $message }}</p>
                         @enderror
                     </div>
 
-                    <div>
-                        <label for="departure">Departure Time</label>
+                    <div class="mb-4">
+                        <label class="block font-bold mb-1" for="departure">Departure Time <span class="text-gray-400">(Required)</span></label>
 
-                        <input id="departure" name="departure" value="{{ old('departure') }}" type="datetime-local">
+                        <input class="border-2 rounded-md px-2 py-1 text-lg" id="departure" name="departure" value="{{ old('departure') }}" type="datetime-local">
 
                         @error('departure')
-                            <p>{{ $message }}</p>
+                            <p class="text-red-400 font-bold text-sm">{{ $message }}</p>
                         @enderror
                     </div>
                 </fieldset>
 
-                <button>Calculate</button>
+                <button class="border-2 rounded-md px-3 py-1.5 font-bold transition hover:border-blue-500 hover:text-blue-500 hover:bg-blue-100">Calculate</button>
             </form>
 
-            @if('earnings')
-                <section>
-                    {{ $earnings ?? null }}
+            @if($earnings ?? false)
+                <section class="max-w-md mx-auto mt-10 text-center border px-24 py-12 rounded-lg">
+                    <p class="text-gray-400 font-bold">Total Earnings:</p>
+                    <p class="text-3xl font-bold">{{ $earnings }}</p>
                 </section>
             @endif
         </main>
